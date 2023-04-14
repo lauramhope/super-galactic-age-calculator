a0d6fc2e48612c058c3eb5961edaebad628f603a
Laura Hope <ssteamlaura@gmail.com>
2023-04-14 09:03:44 -0700

    Add passing JS logic for method to return earth years since previous age.

--- src/js/earthling.js
+++ src/js/earthling.js
@@ -1,6 +1,11 @@
 export default class Earthling {
   constructor(currentEarthAge) {
     this.currentEarthAge = currentEarthAge;
+    this.yearsSince = 0; 
   }
 
+  numEarthYearsSinceAge(previousAge) {
+    let earthYearsPassed = this.currentEarthAge - previousAge; 
+    this.yearsSince = earthYearsPassed; 
+  }
 }
