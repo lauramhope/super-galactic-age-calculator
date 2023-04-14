0b45ae927584ab0e86539e15c298dacb6bcbb1a6
Laura Hope <ssteamlaura@gmail.com>
2023-04-14 09:13:35 -0700

    Add passing JS logic for method returning number of earth years until future earthling birthday.

--- src/js/earthling.js
+++ src/js/earthling.js
@@ -2,10 +2,16 @@ export default class Earthling {
   constructor(currentEarthAge) {
     this.currentEarthAge = currentEarthAge;
     this.yearsSince = 0; 
+    this.yearsUntil = 0;
   }
 
   numEarthYearsSinceAge(previousAge) {
     let earthYearsPassed = this.currentEarthAge - previousAge; 
     this.yearsSince = earthYearsPassed; 
+  }
+
+  numEarthYearsUntilAge(futureAge) {
+    let earthYearsUntil = futureAge - this.currentEarthAge;
+    this.yearsUntil = earthYearsUntil; 
   }
 }
