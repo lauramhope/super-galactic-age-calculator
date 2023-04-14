c364ea064298409e711ca46e0446b1f3c848a602
Laura Hope <ssteamlaura@gmail.com>
2023-04-14 16:39:18 -0700

    Final touches to project before submitting.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Earthling from './js/earthling.js';
-
-window.addEventListener("load", function() {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
-  document.querySelector("#rectangle-area-form").addEventListener("submit", handleRectangleForm);
-});
