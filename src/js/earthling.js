9a14518fe1639f765d601390766f056f21b5c83d
Laura Hope <ssteamlaura@gmail.com>
2023-04-14 15:03:03 -0700

    CORRECTION: add passing JS logic specifying rounding mercury years to 2 decimal places.

--- src/js/earthling.js
+++ src/js/earthling.js
@@ -29,7 +29,7 @@ export default class Earthling {
 
   mercury() {
     let mercuryAgeConversion = this.currentEarthAge / 0.24;
-    this.mercuryAge = parseInt(mercuryAgeConversion);
+    this.mercuryAge = Number(mercuryAgeConversion.toFixed(2));
   }
 
   numMercuryYearsSinceAge(previousAge) {
