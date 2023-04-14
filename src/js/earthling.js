bd6d351374828fe2e0a9c2b508d974c9c37a4918
Laura Hope <ssteamlaura@gmail.com>
2023-04-14 14:29:28 -0700

    Add passing test JS logic for mars years conversion.

--- src/js/earthling.js
+++ src/js/earthling.js
@@ -58,8 +58,8 @@ export default class Earthling {
   }
 
   mars() {
-    let marsAgeConversion = this.currentEarthAge / 1.88;
-    this.marsAge = parseInt(marsAgeConversion); 
+    let marsAgeConversion = this.currentEarthAge / 1.88; 
+    this.marsAge = parseInt(marsAgeConversion);
   }
 
   numMarsYearsSinceAge(previousAge) {
